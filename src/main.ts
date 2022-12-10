ca3bedecbe54106f9e5bc11e0e2300641c055e47
Hélio Souza <contatohelioseverino@gmail.com>
2022-12-10 14:09:35 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionFilter } from './infrastructure/common/filter/exception.filter';
+import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
+import { ResponseInterceptor } from './infrastructure/common/interceptors/response.interceptor';
+import { LoggerService } from './infrastructure/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Filter
+  app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
+  // interceptors
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
